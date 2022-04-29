925f59d1d05ed2ea498e920f5d56d7933fac0b24
robby-rob <99498578+robby-rob@users.noreply.github.com>
2022-04-28 23:15:41 -0700

    airbyte

--- airbyte/source/run.sh
+++ airbyte/source/run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+mkdir /home/yq/output
+
 cd /tmp
 
 wget https://raw.githubusercontent.com/airbytehq/airbyte/master/.env
 wget https://raw.githubusercontent.com/airbytehq/airbyte/master/docker-compose.yaml
 
-cp .env /app/output/.env
+cp .env /home/yq/output/.env
 
 # Backend Network
 cat docker-compose.yaml | \
 yq 'del(.services.*.ports)' | \
 yq '.services.*.networks.[0] = "backend"' | \
 yq '.networks.backend.name = "airbyte-backend"' | \
-cat > /app/output/docker-compose.yaml
+cat > /home/yq/output/docker-compose.yaml
