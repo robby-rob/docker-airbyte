cb4c82479bc7a5f43fe950ba98124e21355b9a0c
Robby Robinson <99498578+robby-rob@users.noreply.github.com>
2022-04-28 22:40:23 -0700

    Revert "changed dir for linux builds"

--- airbyte/source/run.sh
+++ airbyte/source/run.sh
@@ -5,11 +5,11 @@ cd /tmp
 wget https://raw.githubusercontent.com/airbytehq/airbyte/master/.env
 wget https://raw.githubusercontent.com/airbytehq/airbyte/master/docker-compose.yaml
 
-cp .env /home/yq/output/.env
+cp .env /app/output/.env
 
 # Backend Network
 cat docker-compose.yaml | \
 yq 'del(.services.*.ports)' | \
 yq '.services.*.networks.[0] = "backend"' | \
 yq '.networks.backend.name = "airbyte-backend"' | \
-cat > /home/yq/output/docker-compose.yaml
+cat > /app/output/docker-compose.yaml
--- airbyte/update-airbyte.ps1
+++ airbyte/update-airbyte.ps1
@@ -2,6 +2,6 @@ cd source
 docker build -t update-airbyte .
 docker run --name update-airbyte update-airbyte
 cd ..
-docker cp update-airbyte:/home/yq/output/. .
+docker cp update-airbyte:/app/output/. .
 docker container rm update-airbyte
 docker image rm update-airbyte
