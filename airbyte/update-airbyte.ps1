81cae095b5dce46a691d377c942b8b565a6c8725
robby-rob <99498578+robby-rob@users.noreply.github.com>
2022-04-29 00:14:42 -0700

    use sh script

--- airbyte/update-airbyte.ps1
+++ airbyte/update-airbyte.ps1
@@ -1,7 +1,1 @@
-cd source
-docker build -t update-airbyte .
-docker run --name update-airbyte update-airbyte
-cd ..
-docker cp update-airbyte:/home/yq/output/. .
-docker container rm update-airbyte
-docker image rm update-airbyte
+Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Get-Content -Raw .\update-airbyte.sh)))
